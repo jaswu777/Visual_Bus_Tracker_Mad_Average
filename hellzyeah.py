4fa2189c291e77797324a2304876e2e78afd69cb
Jason Wu <jaswu@eden.rutgers.edu>
2014-04-15 17:16:16 -0400

    Boo yeah

--- hellzyeah.py
+++ hellzyeah.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, jsonify, url_for, request
-import json, requests, urllib2, time, datetime, calendar, urllib, re
+import json, requests, urllib2, time, datetime, calendar, urllib, re, geohash
 app = Flask(__name__) 
 
 @app.route("/")
@@ -26,6 +26,8 @@ def third_one():
 	location_total = street + "," + city + "," + state + "," + country
 	google_url = "http://maps.googleapis.com/maps/api/geocode/json?address=" + location_total + "&sensor=false"
 	google_data = json.loads(urllib2.urlopen(google_url).read())
+	if google_data["status"] != "OK":
+		return render_template('ErrorMessage.html', name = name)
 	lat = google_data["results"][0]["geometry"]["location"]["lat"]
 	lon = google_data["results"][0]["geometry"]["location"]["lng"]
 	link = "http://runextbus.herokuapp.com/locations"
@@ -47,7 +49,24 @@ def third_one():
 			bus_name.append(new)
 	#return str(len(latitude))
 	#return jsonify(data_parsed)
-	return render_template('mapmarkers.html', name = name, lat = lat, lon = lon, latitude = latitude, longitude = longitude, bus_name = json.dumps(bus_name))
+	data_link = "http://runextbus.herokuapp.com/active"
+	data_link = json.loads(urllib2.urlopen(data_link).read())
+	#return str(data_link["stops"][0]["geoHash"])
+	#return str(data_link["stops"][0]["title"])
+	#return str(len(data_link["stops"]))
+	#return jsonify(data_link)
+	geolat = []
+	geolon = []
+	bus_stop = []
+	for i in range(0, len(data_link["stops"])):
+		post = data_link["stops"][i]["geoHash"]
+		post = str(post)
+		temp = geohash.decode(post)
+		geolat.append(float(temp[0]))
+		geolon.append(float(temp[1]))
+		hold = data_link["stops"][i]["title"]
+		bus_stop.append(str(hold))
+	return render_template('mapmarkers.html', name = name, lat = lat, lon = lon, latitude = latitude, longitude = longitude, bus_name = json.dumps(bus_name), geolat = geolat, geolon = geolon, bus_stop = json.dumps(bus_stop))
 
 if __name__ == '__main__':
-	app.run(debug = True)
+	app.run(debug = False)
